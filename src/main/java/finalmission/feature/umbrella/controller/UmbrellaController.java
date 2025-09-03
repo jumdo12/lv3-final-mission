8dbe6f58c9389b7799a1e07755e9cfbdc4218123
jumdo12 <jumdo12@gmail.com>
2025-09-03 17:01:31 +0900

    feat: 스웨거 추가

--- src/main/java/finalmission/feature/umbrella/controller/UmbrellaController.java
+++ src/main/java/finalmission/feature/umbrella/controller/UmbrellaController.java
@@ -2,17 +2,21 @@ package finalmission.feature.umbrella.controller;
 
 import finalmission.feature.umbrella.dto.response.UmbrellaResponse;
 import finalmission.feature.umbrella.service.UmbrellaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "우산 API", description = "우산 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class UmbrellaController {
 
     private final UmbrellaService umbrellaService;
 
+    @Operation(summary = "우산 등록", description = "우산을 100개 등록합니다.")
     @PostMapping("/umbrellas")
     public ResponseEntity<Void> registerUmbrella(){
         for(int i=0; i < 100; i++){
